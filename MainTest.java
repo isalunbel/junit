2a661a6812374ac829f431f41ddcdb693b2146e2
isalunbel <142603867+isalunbel@users.noreply.github.com>
2023-11-01 15:16:32 +0300

    Add files via upload

---
+++ MainTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @org.junit.jupiter.api.Test
+    public void testConcat_validArgument_success() {
+        final String original = "new_data.json";
+        final String argument = Main.readString(original);
+        final String expected = "new_data.json[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}," +
+                "{\"id\":2,\"firstName\":\"Inav\",\"lastName\":\"Petrov\",\"country\":\"RU\",\"age\":23}]";
+        final String result = original.concat(argument);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJsonToList() {
+        String original = "new_data.json";
+        List<Employee> result = Main.jsonToList(original);
+        Assertions.assertNotNull(result);
+    }
+}
